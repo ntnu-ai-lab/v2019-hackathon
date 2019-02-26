1d03c730f868e6e90ff41d047f55d8aa59105d89
henrhoi <henrik.hoiness@online.no>
2019-02-26 09:29:05 +0100

    Almost done

--- yr_data.py
+++ yr_data.py
@@ -1,6 +1,8 @@
+import multiprocessing as mp
 from six.moves import urllib
 import datetime
 import re
+import requests
 
 """
 Reading from YR's public weather statistics from Trondheim's weather station 688860, Voll
@@ -22,41 +24,45 @@ def get_datetime(date):
     return datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M')
 
 
-def retrieve_measurements_by_date(date, lines):
+def retrieve_measurements_by_date(args):
+    url, date = args
     measurements, measurement = [], None
 
-    for line in lines:
-        line = line.decode().strip()
+    r = requests.get(url, stream=True)
 
-        time_match = REGEXES['time'].search(line)
-        if time_match:
-            measurement = {"temperature": []}
-            when = get_datetime(f'{date}T{time_match.group(1)}')
-            measurement['timestamp'] = int(when.timestamp() * 1e3)
-            continue
+    for line in r.iter_lines():
+        if line:
+            line = line.decode().strip()
 
-        temperature_match = REGEXES['temperature'].search(line)
-        if temperature_match:
-            measurement['temperature'].append(float(temperature_match.group(1)))
-            continue
+            time_match = REGEXES['time'].search(line)
+            if time_match:
+                measurement = {"temperature": []}
+                when = get_datetime(f'{date}T{time_match.group(1)}')
+                measurement['timestamp'] = int(when.timestamp() * 1e3)
+                continue
 
-        rain_match = REGEXES['rain'].search(line)
-        if rain_match:
-            measurement['precipitation'] = float(rain_match.group(1))
-            continue
+            temperature_match = REGEXES['temperature'].search(line)
+            if temperature_match:
+                measurement['temperature'].append(float(temperature_match.group(1)))
+                continue
 
-        humidity_match = REGEXES['humidity'].search(line)
-        if humidity_match:
-            measurement['humidity'] = int(humidity_match.group(1))
-            measurement['temperature'] = dict(
-                measured=measurement['temperature'][0],
-                max=measurement['temperature'][1],
-                min=measurement['temperature'][2]
-            )
-            measurements.append(measurement)
-            measurement = None
+            rain_match = REGEXES['rain'].search(line)
+            if rain_match:
+                measurement['precipitation'] = float(rain_match.group(1))
+                continue
 
-    return measurements
+            humidity_match = REGEXES['humidity'].search(line)
+            if humidity_match:
+                measurement['humidity'] = int(humidity_match.group(1))
+                measurement['temperature'] = dict(
+                    measured=measurement['temperature'][0],
+                    max=measurement['temperature'][1],
+                    min=measurement['temperature'][2]
+                )
+                measurements.append(measurement)
+                measurement = None
+
+    return {date: measurements}
 
 
 def retrieve_all_measurements(start_date, end_date, location):
@@ -64,27 +70,27 @@ def retrieve_all_measurements(start_date, end_date, location):
     for year in range(start_date.year, end_date.year + 1):
         for month in range(1, end_date.month + 1):
             for day in range(1, end_date.day + 1):
-                urls += [f'{year}-{month}-{day}']
+                urls += [(get_url(location, f'{year}-{month}-{day}'), f'{year}-{month}-{day}')]
 
     print(urls)
+    print("No of urls:", len(urls))
+    pool = mp.Pool()
+    processes = pool.map_async(retrieve_measurements_by_date, urls)
+    pool.close()
+
+    return processes.get()
 
 
 def main():
-    location = 'Norway/Akershus/B%C3%A6rum/Fornebu'
+    location = 'Norway/Tr%C3%B8ndelag/Trondheim/Trondheim/'
     start_date = datetime.datetime(2013, 12, 31)
+    #end_date = datetime.datetime(2014, 1, 1)
     end_date = datetime.datetime(2019, 2, 25)
 
-    retrieve_all_measurements(start_date, end_date, location)
+    all_measurements = retrieve_all_measurements(start_date, end_date, location)
 
-    date = f'{start_date.year}-{start_date.month}-{start_date.day}'
-    with urllib.request.urlopen(get_url(location=location, date=date)) as url:
-        print(get_url(location=location, date=date))
-        lines = url.readlines()
-        print(lines)
-
-    print(retrieve_measurements_by_date(date, lines))
-    return 0
-
+    with open("YR_Dataset_Trondheim_2014_2019.json", "w") as f:
+        f.write(all_measurements)
 
 if __name__ == '__main__':
-    exit(main())
+    main()
